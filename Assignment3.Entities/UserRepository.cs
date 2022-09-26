1fd25e0b46b7c098bc5e71c59850404974da8224
tkjelds <tkjelds385@gmail.com>
2022-09-26 17:58:45 +0200

    added test for rest of user repo

--- Assignment3.Entities/UserRepository.cs
+++ Assignment3.Entities/UserRepository.cs
@@ -29,24 +29,30 @@ public class UserRepository : IUserRepository
         // Check if force is used
         if(force != true) return Response.Conflict;
         // Check if user exists
+        try{
         var _user = _context.Users.Where(u => u.Id == userId).First();
-        if(_user == null) return (Response.Conflict);
         _context.Users.Remove(_user);
         _context.SaveChanges();
         return Response.Deleted;
+        } catch {
+            return (Response.Conflict);
+        }
     }
 
     public UserDTO Read(int userId)
     {
         // Check if user exists
+        try {
         var _user = _context.Users.Where(u => u.Id == userId).First();
-        if(_user == null) return null;
         return (new UserDTO(            
             _user.Id,
             _user.Name,
             _user.Email)
 
         );
+        } catch {
+            return null;
+        }
     }
 
     public IReadOnlyCollection<UserDTO> ReadAll()
@@ -61,13 +67,18 @@ public class UserRepository : IUserRepository
 
     public Response Update(UserUpdateDTO user)
     {
-        // Check if user exists
+        try 
+        {
         var _user = _context.Users.Where(u => u.Id == user.Id).First();
-        if(_user == null) return Response.Conflict;
         _user.Email = user.Email;
         _user.Name = user.Name;
         _context.Users.Update(_user);
         _context.SaveChanges();
         return Response.Updated;
+        } 
+        catch
+        {
+        return Response.Conflict;
+        }
     }
 }
