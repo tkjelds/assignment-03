93c7092e3ae3eaa23c2db6938e1fb741596d915b
tkjelds <tkjelds385@gmail.com>
2022-09-26 12:21:05 +0200

    added crud for tags and users, along with tests for tags

--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -2,28 +2,64 @@ namespace Assignment3.Entities;
 
 public class TagRepository : ITagRepository
 {
+    private readonly KanbanContext _context;
+
+    public TagRepository(KanbanContext context)
+    {
+        _context = context;
+    }
     public (Response Response, int TagId) Create(TagCreateDTO tag)
     {
-        throw new NotImplementedException();
+        try 
+        {
+            _context.Tags.Add(new Tag{
+                Name = tag.Name
+            });
+            _context.SaveChanges();
+            var tagId = _context.Tags.Where(t => t.Name == tag.Name).First().Id;
+            return (Response.Created,tagId);
+        } 
+        catch 
+        {
+            return (Response.Conflict, -1);
+        } 
     }
 
     public Response Delete(int tagId, bool force = false)
     {
-        throw new NotImplementedException();
+        if(force != true) return Response.Conflict;
+        var tag = _context.Tags.Where(t=> t.Id == tagId).First();
+        if(tag == null) return Response.NotFound;
+        _context.Tags.Remove(tag);
+        _context.SaveChanges();
+        return Response.Deleted;
     }
 
     public TagDTO Read(int tagId)
     {
-        throw new NotImplementedException();
+        var tag = _context.Tags.Where(t => t.Id == tagId).First();
+        if (tag == null) return null;
+        TagDTO _tagDTO = new TagDTO (tag.Id,tag.Name);
+        return _tagDTO;
     }
 
     public IReadOnlyCollection<TagDTO> ReadAll()
     {
-        throw new NotImplementedException();
+        List<TagDTO> tagDTOs = new List<TagDTO> {};
+        foreach (var item in _context.Tags)
+        {
+            tagDTOs.Add(new TagDTO(item.Id,item.Name));
+        }
+        return tagDTOs;
     }
 
     public Response Update(TagUpdateDTO tag)
     {
-        throw new NotImplementedException();
+        var _tag = _context.Tags.Where(t => t.Id == tag.Id).First();
+        if(_tag == null) return Response.NotFound;
+        _tag.Name = tag.Name;
+        _context.Tags.Update(_tag);
+        _context.SaveChanges();
+        return Response.Updated;
     }
 }
--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -2,14 +2,40 @@ namespace Assignment3.Entities;
 
 public class TaskRepository : ITaskRepository
 {
+    private readonly KanbanContext _context;
+
+    public TaskRepository(KanbanContext context)
+    {
+        _context = context;
+    }
     public (Response Response, int TaskId) Create(TaskCreateDTO task)
     {
+        // IEnumerable<Tag> _tags() {
+        //     foreach (var tag in task.Tags)
+        //     {
+        //     yield return  _context.Tags.Where(t => t.Name == tag).First();
+        //     }} 
+        // Task newTask = new Task {
+        //     Title = task.Title,
+        //     AssignedTo = _context.Users.Where(u => task.AssignedToId == u.Id).First(),
+        //     Description = task.Description,
+        //     Tags = _tags()
+        // };
         throw new NotImplementedException();
     }
 
     public Response Delete(int taskId)
     {
-        throw new NotImplementedException();
+        var task = _context.Tasks.Where(t => t.Id == taskId).FirstOrDefault();
+        if (task == null) return Response.BadRequest;
+        if(task.State == State.Active){
+            task.State = State.Removed;
+            _context.Update(task);
+        }
+        if (task.State != State.New) return Response.Conflict;
+        _context.Remove(task);
+        _context.SaveChanges();
+        return Response.Deleted;
     }
 
     public TaskDetailsDTO Read(int taskId)
