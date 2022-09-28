d92502f464350fbbfe7a869c9fb843db33d216cb
tkjelds <tkjelds385@gmail.com>
2022-09-28 10:38:36 +0200

    added datetime for tasks

--- Assignment3.Entities/Migrations/20220928083612_updateTasks.Designer.cs
+++ Assignment3.Entities/Migrations/20220928083612_updateTasks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Assignment3.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Assignment3.Entities.Migrations
 {
     [DbContext(typeof(KanbanContext))]
-    partial class KanbanContextModelSnapshot : ModelSnapshot
+    [Migration("20220928083612_updateTasks")]
+    partial class updateTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.9");
